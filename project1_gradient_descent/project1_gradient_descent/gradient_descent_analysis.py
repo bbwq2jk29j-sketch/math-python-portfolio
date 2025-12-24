d9505392090acbbb711edb487bfc6d6fe78ef3ef
bbwq2jk29j-sketch <bbwq2jk29j@privaterelay.appleid.com>
2025-12-24 15:10:44 +0900

    Update gradient_descent_analysis.py

--- project1_gradient_descent/project1_gradient_descent/gradient_descent_analysis.py
+++ project1_gradient_descent/project1_gradient_descent/gradient_descent_analysis.py
@@ -1,1 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+def f(x):
+  return x**2 - 4*x + 5
+
+x = np.linspace(-1, 5, 100)
+y = f(x)
+
+plt.plot(x,y)
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.title("Function Visualization")
+plt.show()
